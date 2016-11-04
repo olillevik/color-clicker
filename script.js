b97d4f9da6e98f2ed5c5a0e8e8ee87145a5e9330
Ørjan Markhus Lillevik <olillevik@gmail.com>
2016-11-04 21:44:32 +0100

    Multiple changes

--- script.js
+++ script.js
@@ -1,40 +1,46 @@
 (function () {
     'use strict';
 
-    //modell
-    //liste med farger
-    //nåværende farge
-    //timer
-    //poengteller
+    //data
+    var farger = ["red", "yellow", "blue"];
+    var tekst = ["RØD", "GUL", "BLÅ"];
+    var poeng = 0;
+    var slutt = false;
 
     var el = function (id) {
         return document.getElementById(id + "");
     };
 
+    var settNyFarge = function () {
+        el("farge").style.color = farger[Math.random() * 3];
+        el("farge").style.color = tekst[Math.random() * 3];
+    }
+
     //oppdater modell
-    var oppdater = function (timeout) {
-        var knappTrykket = this.id;
-        console.log("Knapp " + this.id + " trykket");
+    var knappTrykket = function () {
+        if (slutt === false) {
+            console.log("Knapp " + this.id + " trykket");
 
-        //if knappTrykket.color === current.color
-        //points.counter++
+            var valgtFarge = el(this.id).style.color;
+            var riktigFarge = el("farge").style.color;
 
-        //refresh current
+            if (valgtFarge === riktigFarge) {
+                poeng = poeng + 1;
+            }
+        }
+        settNyFarge();
+    }
 
-        vis();
+    var timeout = function () {
+        slutt = true;
     };
 
-    //oppdater visning
-    var vis = function (id) {
-        //tegn nåværende farge
-        //oppdater score
-    }
-
     //inputs
-    for (var feltId = 0; feltId <= 5; feltId++) {
-        el(feltId).addEventListener("click", oppdater);
+    for (var feltId = 0; feltId <= 2; feltId++) {
+        el(feltId).addEventListener("click", knappTrykket);
     }
 
-    //kall oppdater med timeout
+    window.setInterval(timeout, 100000);
 
+    settNyFarge();
 })();
